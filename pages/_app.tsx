4ec2ce84efededb7eb956e9d3b0338bdd557d6dc
Antonio Hickey <64357439+antonio-hickey@users.noreply.github.com>
2022-11-24 00:41:21 -0500

    feat: trpc for end to end typesafety

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { trpc } from '../utils/trpc';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
+
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
+
+
+export default trpc.withTRPC(App);
